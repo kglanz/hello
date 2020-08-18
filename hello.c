462664378b9e6b0537786a4e3e1bc4d593e19f6e
kglanz <s-kglanz1@huskers.unl.edu>
2020-08-17 19:51:34 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Kris Glanz
+ *         kglanz@huskers.unl.edu
+ * Date: 2020/08/16
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Kris!\n");
+
+  return 0;
+}
